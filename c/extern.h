fb28ed9dff59c7c3d8525ee48c2959457ec14c85
aaronryank <rkcoder2002@gmail.com>
2016-12-06 22:19:34 +0000

    Create placeholder for infection

--- c/extern.h
+++ c/extern.h
@@ -1,1 +1,5 @@
+/* simulation.c functions */
 extern void run_simulation(node_t **);
+
+/* _kbhit.c function */
+extern int _kbhit(void);
--- c/simulation.c
+++ c/simulation.c
@@ -1,15 +1,19 @@
 #include "node.h"
 #include "cell.h"
-#include "color.h"
+#include "defs.h"
+#include "extern.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int Total_rounds = 0;
 
+/* display stats about simulation */
 void print_data(node_t *cells)
 {
     node_t *current = cells;
     int healthy_cells, infected_cells;
 
+    /* update healthy and infected cell counts */
     while (current != NULL) {
         if (healthy(current->val))
             healthy_cells++;
@@ -18,16 +22,25 @@ void print_data(node_t *cells)
 	current = current->next;
     }
 
+    /* display data */
     clear_screen();
     printf("Round %d (%d Cell%s)\n", Total_rounds, Total_cells, (Total_cells == 1) ? "" : "s");
     printf("Healthy: %d Infected: %d\n", healthy_cells, infected_cells);
 
 #ifdef __TEST__
     print_list(*cells);
-#endif // __TEST__
+#endif
+
+    /* wait, check if 'i' is pressed
+     * if so, begin infection        */
+    sleep(1);
+    if (_kbhit() == 'i') {
+        /* add infect() function */
+    }
 
 }
 
+/* self-explanatory; run the simulation */
 void run_simulation(node_t **cells)
 {
     Total_rounds++;
